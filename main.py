026f69ac5986a2678d3b616fb2e0133151b7e248
jxmend <jonas.msilva.jms@gmail.com>
2025-11-12 11:27:29 -0300

    Tarefa 5: Prepare um pedido de sorvete

--- main.py
+++ main.py
@@ -12,33 +12,43 @@ class TestUrbanRoutes:
             print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
 
     def test_set_route(self):
+        # Adicionar em S8
         print("função criada para definir a rota")
         pass
 
     def test_select_plan(self):
+        # Adicionar em S8
         print("função criada para selecionar o plano")
         pass
 
     def test_fill_phone_number(self):
+        # Adicionar em S8
         print("função criada para preencher número de telefone")
         pass
 
     def test_fill_card(self):
+        # Adicionar em S8
         print("função criada para preencher cartão")
         pass
 
     def test_comment_for_driver(self):
+        # Adicionar em S8
         print("função criada para comentário ao motorista")
         pass
 
     def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
         print("função criada para pedir manta e lenços")
         pass
 
     def test_order_2_ice_creams(self):
+        # Adicionar em S8
+        for i in range(2):
+            # Adicionar em S8
+            pass
         print("função criada para pedir 2 sorvetes")
-        pass
 
     def test_car_search_model_appears(self):
+        # Adicionar em S8
         print("função criada para verificar modelo do carro na busca")
         pass
