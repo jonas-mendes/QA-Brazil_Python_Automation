220e11badee37c6681e268ed3b98ad5d20dc8630
jxmend <jonas.msilva.jms@gmail.com>
2025-11-12 11:23:38 -0300

    Tarefa 4: Verifique se o servidor está funcionando

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 # Arquivo com constantes
 
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-a2d65266-a28c-4397-95c2-a0fd66037fca.containerhub.tripleten-services.com?lng=pt'
 
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
--- main.py
+++ main.py
@@ -1,2 +1,44 @@
+import data
+import helpers
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        # Verifica se o servidor está funcionando
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+
+    def test_set_route(self):
+        print("função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        print("função criada para selecionar o plano")
+        pass
+
+    def test_fill_phone_number(self):
+        print("função criada para preencher número de telefone")
+        pass
+
+    def test_fill_card(self):
+        print("função criada para preencher cartão")
+        pass
+
+    def test_comment_for_driver(self):
+        print("função criada para comentário ao motorista")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        print("função criada para pedir manta e lenços")
+        pass
+
+    def test_order_2_ice_creams(self):
+        print("função criada para pedir 2 sorvetes")
+        pass
+
+    def test_car_search_model_appears(self):
+        print("função criada para verificar modelo do carro na busca")
+        pass
